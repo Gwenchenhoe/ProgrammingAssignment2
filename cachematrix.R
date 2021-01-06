6ce7ff949f40395333414d93150790b102ac2322
Gwenchenhoe <gsriego@mymail.mapua.edu.ph>
2021-01-06 23:01:12 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,41 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This Assignment enables to test two functions
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function()x
+  setinv <- function(inverse)inv <<- inverse
+  getinv <- function() {
+    inv<-ginv(x)
+    inv%*%x
+  }
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
-
 
 ## Write a short comment describing this function
+## In this part, determining the cached data
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  inv <- x$getinv()
+  if (!is.null(inv)) {
+    message("getting cached data!")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setinv(inv)
+  inv
 }
+
